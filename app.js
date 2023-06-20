7179a074a6a01ecfb0e4aae4c9dde03019053225
Atauba Gideon <86924003+Gidii04@users.noreply.github.com>
2023-06-20 22:01:48 +0100

    add all files

---
+++ app.js
@@ -0,0 +1,10 @@
+const mobileOverlay = document.querySelector('.mobile-overlay')
+const mobileBar = document.querySelector('.mobile-bars')
+const closeBar = document.querySelector('.close-bar')
+mobileBar.addEventListener('click', () => {
+  mobileOverlay.classList.add('show-mobile-overlay')
+})
+
+closeBar.addEventListener('click', () => {
+  mobileOverlay.classList.remove('show-mobile-overlay')
+})
